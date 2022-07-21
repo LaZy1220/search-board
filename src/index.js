eaac15e516c358541cb75bb0ce405e91e2cb6a93
LaZy1220 <4elove4iska@gmail.com>
2022-07-21 12:15:16 +0300

    add adaptive and redux-persist

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {Provider} from 'react-redux'
-import { store } from './store/index';
+import { store,persistor } from './store/index';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App/>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,15 @@
 import { createStore } from "redux";
 import { rootReducer } from "./rootReducer";
+import { persistStore,persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { devToolsEnhancer } from "redux-devtools-extension";
 
-export const store = createStore(rootReducer,devToolsEnhancer())
+const persistConfig = {
+    key:'root',
+    storage,
+    //whitelist:[],
+    //blacklist:[],
+}
+const persistedReducer = persistReducer(persistConfig,rootReducer)
+export const store = createStore(persistedReducer,devToolsEnhancer())
+export const persistor = persistStore(store)
