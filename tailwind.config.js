c45e94d5d74bc0b07c6535cc1c51de94a3b85582
LaZy1220 <4elove4iska@gmail.com>
2022-09-30 16:57:02 +0300

    remove Tailwind and install styled

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,7 @@ module.exports = {
     },
     extend: {
       colors:{
-        lightdarkgray:'hsl(180, 29%, 50%)',
-        lightgray:'hsl(180, 52%, 96%)',
-        darkgray:'hsl(180, 8%, 52%)',
-        verydarkgray:'hsl(180, 14%, 20%)'
+       
       },
     },
   },
