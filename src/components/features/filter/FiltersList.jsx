40e3e1af6972d9969d820702f405591904ebecc6
LaZy1220 <4elove4iska@gmail.com>
2022-10-03 15:47:43 +0300

    refact on RTK  Filter

---
+++ src/components/features/filter/FiltersList.jsx
@@ -0,0 +1,45 @@
+import { useSelector,useDispatch } from "react-redux/es/exports"
+import { clearFilter,removeFilter } from "../../../store/filters/filters-action"
+import {selectFilters} from '../../../store/filters/filters-selector'
+import {
+    FilterListConteiner,
+    FilterListEl,
+    FilterListUl,
+    Filter,
+    FilterName,
+    Icon,
+    Clear
+} from '../../styled/FilterListStyles'
+
+export function FilterList(){
+    const filters = useSelector(selectFilters)
+    const dispatch = useDispatch()
+    if (filters.length===0){
+        return null
+    }
+    return(
+        <FilterListConteiner>
+            <FilterListEl>
+                <FilterListUl>
+                {
+                    filters.map(filter=>
+                        <Filter key={filter}>
+                            <FilterName>{filter}</FilterName>
+                            <Icon
+                                onClick={()=>dispatch(removeFilter(filter))}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"         stroke="currentColor" strokeWidth={2}>
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" 
+                                    viewport/>
+                                    </svg>
+                            </Icon>
+                        </Filter>)
+                }
+                </FilterListUl>
+            <Clear className="text-blue-400 cursor-pointer mr-10 underline decoration-2 ml-3"
+                onClick={()=>dispatch(clearFilter())}>
+                    Clear
+             </Clear>
+            </FilterListEl>
+        </FilterListConteiner>
+    )
+}
