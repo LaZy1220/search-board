3e8424b231de37b166631e1f0cf71f34816c9788
LaZy1220 <4elove4iska@gmail.com>
2022-10-05 20:25:16 +0300

    Add pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import {useEffect} from 'react'
+import {Routes, Route} from 'react-router-dom'
 import {FilterList} from './components/features/filter/FiltersList'
 import { JobsList } from './components/features/positions/JobsList';
-import {useDispatch} from  'react-redux'
-import {queryPositions} from './components/features/positions/position-slice'
 import { Header } from './components/Header';
 
 function App() {
-  const dispatch = useDispatch()
-  useEffect(()=>{
-    dispatch(queryPositions())
-    },[])
   return (
     <div className="App">
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+      </Routes>
       <Header />
       <FilterList/>
       <JobsList/>
