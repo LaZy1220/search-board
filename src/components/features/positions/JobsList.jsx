9e43dde6bffe55427909699273b00f8d1ce678db
LaZy1220 <4elove4iska@gmail.com>
2022-10-28 19:32:09 +0300

    add somechanges

--- src/components/features/positions/JobsList.jsx
+++ src/components/features/positions/JobsList.jsx
@@ -1,25 +1,29 @@
-import { Card } from './Card'
-import {useDispatch} from 'react-redux'
-import { addFilter} from '../filter/filter-slice'
-import { usePositions } from './use-positions'
-import { useFetchPositions } from './use-fetch-positions'
+import { Card } from "./Card";
+import { useDispatch, useSelector } from "react-redux";
+import { addFilter } from "../filter/filter-slice";
+import { useEffect } from "react";
+import { queryPositions } from "./position-slice";
+import { selectVisiblePositions } from "./position-slice";
+import { selectFilters } from "../filter/filter-slice";
 
-export function JobsList(){
-    useFetchPositions()
-    const dispatch = useDispatch()
-    const positions = usePositions()
-    const handleAddFilter = (filter)=>{
-        dispatch(addFilter(filter))
-    }
-    return(
-        <>
-            {positions.map(item => (
-              <Card
-            key={item.id}
-            handleAddFilter={handleAddFilter}
-            {...item}
-             />
-            ))}
-        </>
-    )
+export function JobsList() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    console.log("effect");
+    dispatch(queryPositions());
+  }, [dispatch]);
+  const handleAddFilter = (filter) => {
+    dispatch(addFilter(filter));
+  };
+  const currentFilters = useSelector(selectFilters);
+  const positions = useSelector((state) => {
+    selectVisiblePositions(state, currentFilters);
+  });
+  return (
+    <>
+      {positions?.map((item) => (
+        <Card key={item.id} handleAddFilter={handleAddFilter} {...item} />
+      ))}
+    </>
+  );
 }
