225312182e25db329c7bc75635764be01edb4ee4
LaZy1220 <4elove4iska@gmail.com>
2022-10-30 19:05:00 +0300

    hate redux!!!

--- src/components/features/positions/position-slice.js
+++ src/components/features/positions/position-slice.js
@@ -1,46 +1,38 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const initialState = {
-  positions: [],
-};
-export const queryPositions = createAsyncThunk(
-  "@@position/queryPositions",
-  async (_, { rejectWithValue, dispatch }) => {
-    const response = await axios.get("https://makser-test.site/api/v1/work/");
-    dispatch(addPositions(response.data.results));
+export const loadPositions = createAsyncThunk(
+  "@@positions/load-positions",
+  async (_, { dispatch }) => {
+    const res = await axios.get("https://makser-test.site/api/v1/work/");
+    dispatch(setPositions(res.data));
   }
 );
+
 const positionSlice = createSlice({
   name: "@@positions",
-  initialState,
+  initialState: [],
   reducers: {
-    addPositions: (state, action) => {
+    setPositions: (state, action) => {
       state.positions = action.payload;
     },
   },
-  extraReducers: {
-    [queryPositions.fulfilled]: () => console.log("fulfilled"),
-    [queryPositions.pending]: () => console.log("pending"),
-    [queryPositions.rejected]: () => console.log("rejected"),
-  },
 });
-
-export const { addPositions } = positionSlice.actions;
 export const positionReducer = positionSlice.reducer;
+export const { setPositions } = positionSlice.actions;
 
 export const selectAllPositions = (state) => state.positions;
-export const selectVisiblePositions = (state, filters = []) => {
-  if (filters.length === 0) {
-    return state.positions;
-  }
-  return state.positions.filter((pos) => {
-    const positionsFilters = [].concat(
-      ...pos.languages,
-      ...pos.tools,
-      pos.position.role,
-      pos.position.level
-    );
-    return filters.every((filter) => positionsFilters.includes(filter));
-  });
-};
+// export const selectVisiblePositions = (state, filters = []) => {
+//   if (filters.length === 0) {
+//     return state.positions;
+//   }
+//   return state.positions.filter((pos) => {
+//     const positionsFilters = [].concat(
+//       ...pos.languages,
+//       ...pos.tools,
+//       pos.position.role,
+//       pos.position.level
+//     );
+//     return filters.every((filter) => positionsFilters.includes(filter));
+//   });
+// };
